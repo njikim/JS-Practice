5eabdbee1245b2a7f406dd89b84e55567041af92
njikim <eunjikim214@gmail.com>
2019-04-24 15:36:33 +0900

    delete todo from html & local storage

--- Clock_ToDo/todo.js
+++ Clock_ToDo/todo.js
@@ -4,14 +4,14 @@ const toDoForm = document.querySelector(".js-toDoform"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
 
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
     if(loadedToDos !== null){
         const parsedToDos = JSON.parse(loadedToDos);
         parsedToDos.forEach(function(toDo){
-            paintToDos(toDo.text)
+            paintToDo(toDo.text)
         })
     }
 }
@@ -19,16 +19,17 @@ function loadToDos(){
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
-    paintToDos(currentValue);
+    paintToDo(currentValue);
     toDoInput.value = ""
 }
 
-function paintToDos(text){
+function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
@@ -48,6 +49,20 @@ function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));  // can only save string in local storage. change js object (toDos) to string
 }
 
+function deleteToDo(){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function filterFn(toDo){
+    return toDo.id === 1
+}
 function init(){
     loadToDos();
     toDoForm.addEventListener("submit", handleSubmit);
