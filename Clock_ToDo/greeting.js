073df63d0a3539f2aaa0f4c27bcf4b00a2b9e1f8
njikim <eunjikim214@gmail.com>
2019-04-24 14:43:29 +0900

    changed class name from js-form to js-greetingform

--- Clock_ToDo/greeting.js
+++ Clock_ToDo/greeting.js
@@ -1,4 +1,4 @@
-const form = document.querySelector(".js-form"),
+const form = document.querySelector(".js-greetingform"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
