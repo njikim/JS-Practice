b3e39b8e1f15e9813de66c7bb4830b20194dc374
njikim <eunjikim214@gmail.com>
2019-04-24 17:11:54 +0900

    weather using api

--- Clock_ToDo/weather.js
+++ Clock_ToDo/weather.js
@@ -1,11 +1,14 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "de0bf474100dddbeca5e6102da26234d";
 const COORDS = "coords";
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude)
     }
 }
 
@@ -24,6 +27,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -32,6 +36,18 @@ function handleGeoError(){
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
 }
 
 function init(){
