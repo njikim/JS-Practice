9f5da1b055e44397bdb00be3256aecb38a8d9ae7
njikim <eunjikim214@gmail.com>
2019-04-26 16:06:26 +0900

    line size

--- Paint_JS/app.js
+++ Paint_JS/app.js
@@ -1,12 +1,16 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
 canvas.width = 700;
 canvas.height = 700;
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 //clientX,Y: 윈도우 전체의 범위 내에서 마우스 위치값
 //offsetX,Y: 캔버스 내에서 마우스 위치값
@@ -22,7 +26,6 @@ function onMouseMove(event){
     }
 }
 
-
 function stopPainting(){
     painting = false;
 }
@@ -36,6 +39,21 @@ function handleColorClick(event){
     ctx.strokeStyle = color; //override
 }
 
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill"
+    } else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -43,5 +61,16 @@ if(canvas){
     canvas.addEventListener("mouseleave", stopPainting);
 }   
 
+// line color
 Array.from(colors).forEach(
     color => color.addEventListener("click", handleColorClick))
+
+// line size
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+// fill
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
